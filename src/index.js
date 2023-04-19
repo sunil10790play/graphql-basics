b1c0bc727ce71e673f96c87798be20e1b3d42e0e
sunil10790play <sunil10790play@gmail.com>
2023-04-19 08:09:21 +0530

    Create a graphql api. Server with graphql-yoga

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-import myAddFunction, { subtract } from "./math";
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
 
-console.log(myAddFunction(3, 18))
-console.log(subtract(3, 7))
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'My first Graphql query'
+        },
+        name() {
+            return 'Mogambo'
+        },
+        location() {
+            return 'Hell'
+        },
+        bio() {
+            return 'I am a villan with a evil laugh. Buhuhahahaha!'
+        }
+    }
+}
+
+const schema = createSchema({
+    typeDefs,
+    resolvers
+})
+const yoga = createYoga({
+    schema
+})
+const server = createServer(yoga)
+server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+})
--- src/math.js
+++
@@ -1,7 +0,0 @@
-// Default Export
-const add = (x, y) => x + y
-
-// Named Export
-const subtract = (x, y) => x - y
-
-export { add as default, subtract }
